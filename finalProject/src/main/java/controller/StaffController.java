f28a61e7af17a3b118c5bf8e349f95d1be88bbc8
yongguk <kook7749@naver.com>
2018-11-15 00:05:21 +0900

    pom 및 리포시토리, 맵퍼, 콘피그 수정

--- finalProject/src/main/java/controller/StaffController.java
+++ finalProject/src/main/java/controller/StaffController.java
@@ -5,6 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class StaffController {
-
+	
+	@RequestMapping(value="/product", method = RequestMethod.GET)
+	public String handleStep1(Model model) {
+		return "product/productinsert";
+	}
 }
